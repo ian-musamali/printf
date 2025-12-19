ed6d9cf0a04efdc8369ea5afd2ae478a199e06fd
Ian Musamali <ian.musamali12@gmail.com>
2025-12-19 18:39:58 -0500

    Update printf.cpp

--- printf.cpp
+++ printf.cpp
@@ -2,11 +2,10 @@
 
 void printUserInfo()
 {
-    //WRITE CODE BELOW THIS LINE
     std::string name;
     int age;
+    
     std::cin >> name >> age;
     std::cout << "Hello, " << name << "! You are " << age << " years old.";
-    //WRITE CODE UPPER THAN THIS LINE
     
 }
