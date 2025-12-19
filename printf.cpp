3c272ec19bc5e3b4a731bad3187c417f5a45ee78
Ian Musamali <ian.musamali12@gmail.com>
2025-12-19 18:38:34 -0500

    Create printf.cpp

---
+++ printf.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void printUserInfo()
+{
+    //WRITE CODE BELOW THIS LINE
+    std::string name;
+    int age;
+    std::cin >> name >> age;
+    std::cout << "Hello, " << name << "! You are " << age << " years old.";
+    //WRITE CODE UPPER THAN THIS LINE
+    
+}
